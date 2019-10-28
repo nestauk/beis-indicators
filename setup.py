74891b3923bbc98a79f1e6eeeca7073cbebe7909
George Richardson <georgerichardson@posteo.net>
2019-10-28 15:16:14 +0000

    Setup Nesta Data Science cookiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='beis_indicators',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Regional indicators for BEIS.',
+    author='Nesta Innovation Mapping',
+    license='MIT',
+)
