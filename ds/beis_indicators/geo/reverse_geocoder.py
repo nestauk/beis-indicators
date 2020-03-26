39e60a53d9904702c2d7f960706e3b501d3a117a
Juan-Mateos <juan.mateos-garcia@nesta.org.uk>
2020-03-26 16:47:21 +0000

    Scripts for reverse geocoding applied to universities

---
+++ ds/beis_indicators/geo/reverse_geocoder.py
@@ -0,0 +1,113 @@
+import os
+import requests
+import geopandas as gp
+import pandas as pd 
+from zipfile import ZipFile
+import beis_indicators
+import json
+import os
+from io import StringIO, BytesIO
+from shapely.geometry import Point
+
+project_dir = beis_indicators.project_dir
+
+def get_shape(file_name,path):
+    '''
+    Utility function to extract and the shapefile
+    
+    Arguments:
+        url: url for the shapefile zip
+        file_name: name of the file where we want to extract the data
+    
+    '''
+
+    #Do we need to get the data or is it already there?
+
+    shape_names = os.listdir(f'{project_dir}/data/raw/shapefiles')
+
+    if file_name not in shape_names:
+
+        #Get the data
+        print(f'getting {file_name}...')
+
+        #Get url
+        url = shape_lookup[file_name]
+
+        #Request data
+        req = requests.get(url)
+        
+        #Parse the content
+        z = ZipFile(BytesIO(req.content))
+        
+        #Save
+        print(f'saving {file_name}...')
+        z.extractall(f'{path}{file_name}')
+
+    else:
+        print(f'{file_name} already collected')
+
+
+def reverse_geocode(place_df,shape_name,
+                     shape_file,
+                     place_id,
+                     coord_names= ['longitude','latitude']):
+    '''
+    The reverse geocoder takes a df with geographical coordinates and does a spatial merge with a shapefile.
+    
+    Args:
+        place_df (df). A dataframe where every row is an entity we want to reverse geocode
+        shape_name (str): The name of the shapefile we used
+        shape_file (str) the name of the actual shapefile.
+        place_id (str): the name of the variable with the place id in place dfs
+        coord_names (list): Names for the lon and lat variables in the place_df
+        
+    Returns:
+        A spatially merged df with the location ids and their 
+    
+    
+    '''
+    
+    #Read the shapefile    
+    print('Reading shapefile...')
+    
+    source = f'{project_dir}/data/raw/shapefiles/{shape_name}/{shape_file}'
+    
+    #Do we need to unzip it? 
+    #This will be the case with the eurostat files
+    if 'zip' in shape_file.lower():
+        
+    
+        #Here we are removing the .shp.zip 
+        target = f'{project_dir}/data/raw/shapefiles/{shape_name}/{shape_file}'.split('.')[0]
+        
+        with ZipFile(source,'r') as infile:
+                infile.extractall(target)
+        
+        #Get the shapefile name
+        sh = [x for x in os.listdir(target) if '.shp' in x.lower()][0]
+        
+        shape = gp.read_file(f'{target}/{sh}')
+              
+    else:
+        shape = gp.read_file(f'{target}/{sh}')
+        
+              
+    
+    #Change its projection so it can deal with lats and lons
+    shape = shape.to_crs("EPSG:4326")
+    
+    
+    #Create a place_holder df (ho ho) where the index is the place id
+    place_holder = gp.GeoDataFrame(index=place_df[place_id],crs="EPSG:4326")
+    
+    #Create the geo field for spatial merge using Point
+    place_holder['geometry'] = [Point(x[coord_names[0]],x[coord_names[1]]) for rid,x in place_df.iterrows()]
+    
+    print('Joining...')
+    
+    #Spatial join: looks for points inside the polygons
+    joined = gp.sjoin(place_holder,shape,op='within')
+    
+    #Return the joined df
+    return(joined)
+
---
+++ ds/beis_indicators/geo/university_reverse_geocode.py
@@ -0,0 +1,45 @@
+# Script to reverse geocode universities
+
+import requests
+import geopandas as gp
+import pandas as pd 
+from zipfile import ZipFile
+import beis_indicators
+import json
+import os
+from io import StringIO, BytesIO
+from shapely.geometry import Point
+
+#Import the functions for reverse geocoding
+from beis_indicators.geo.reverse_geocoder import *
+
+project_dir = beis_indicators.project_dir
+
+#Load the shapefile lookup
+with open(f'{project_dir}/data/raw/shapefile_urls.json','r') as infile:
+    shape_lookup = json.load(infile)
+
+#Get all the shapefiles
+for name in shape_lookup.keys():
+    get_shape(name,path=f'{project_dir}/data/raw/shapefiles/')
+
+#University metadata
+uni_meta = pd.read_csv(
+                       'http://learning-provider.data.ac.uk/data/learning-providers-plus.csv')
+#Reverse geocoding
+rcs = [reverse_geocode(place_df=uni_meta,
+                        shape_name=shape_n,
+                        shape_file=shape_f,
+                        place_id='UKPRN',
+                        coord_names= ['LONGITUDE','LATITUDE'])['NUTS_ID'] for shape_n,shape_f in 
+zip(['nuts2_2013','nuts2_2016'],
+    ['NUTS_RG_01M_2013_4326_LEVL_2.shp.zip','NUTS_RG_01M_2016_4326_LEVL_2.shp.zip'])]
+
+uni_nuts = pd.concat(rcs,axis=1)
+
+uni_nuts.columns = ['nuts2_2013','nuts_2016']
+
+with open(f'{project_dir}/data/interim/uni_geos.json','w') as outfile:
+    json.dump(uni_nuts.to_dict(orient='index'),outfile)
+
+
