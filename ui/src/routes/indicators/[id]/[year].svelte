6a1b9c6d7b54237e99cf519f00ec7aa14e41e74e
Luca Bonavita <mindrones@users.noreply.github.com>
2020-04-06 20:21:21 +0100

    Implement the map tooltip
    + enable barchart interactivity for cross referencing the two charts
    
    Closes #189

--- ui/src/routes/indicators/[id]/[year].svelte
+++ ui/src/routes/indicators/[id]/[year].svelte
@@ -26,14 +26,17 @@
 	import {format} from 'd3-format';
 	import {scaleQuantize} from 'd3-scale';
 	import {interpolateWarm} from 'd3-scale-chromatic';
+	import {writable} from 'svelte/store';
 	import ChoroplethDiv from '@svizzle/choropleth/src/ChoroplethDiv.svelte';
 	import BarchartV from '@svizzle/barchart/src/BarchartV.svelte';
 	import {
 		applyFnMap,
 		getValue,
 		inclusiveRange,
-		keyValueArrayToObject
+		keyValueArrayToObject,
+		mergeObj
 	} from '@svizzle/utils';
+	import {makeStyle, toPx} from '@svizzle/dom';
 
 	import yearlyKeyToLabel from 'app/data/NUTS2_UK_labels';
 	import {lookup} from 'app/data/groups';
@@ -49,27 +52,40 @@
 		)
   ])(lookup);
 
+	const getNutsId = _.getKey('nuts_id');
+
+	const makeValueAccessor = id => _.pipe([_.getKey(id), Number]);
 	const makeItemsWithId = id => _.pipe([
 		_.mapWith(applyFnMap({
-			key: _.getKey('nuts_id'),
-			value: _.pipe([_.getKey(id), Number])
+			key: getNutsId,
+			value: makeValueAccessor(id)
 		})),
 		_.sortWith([_.sorterDesc(getValue)])
 	]);
 
 	const colorRange = _.map(inclusiveRange([0, 1, 0.2]), interpolateWarm);
 	let colorScale = scaleQuantize().range(colorRange);
+	let selectedKeys = [];
 
 	export let data;
 	export let id;
 	export let year;
+	export let width;
+	export let height;
 
 	$: $selectedYearStore = Number(year);
+	$: getIndicatorValue = makeValueAccessor(id);
 	$: $availableYearsStore = inclusiveRange($lookupStore[id].year_range)
 	$: data && lookupStore.update(_.setPath(`${id}.data`, data));
 	$: description_short = $lookupStore[id].description_short;
 	$: indicatorData = $lookupStore[id].data;
 	$: yearData = indicatorData && indicatorData.filter(obj => obj.year === year);
+	$: makeKeyToValue = _.pipe([
+		_.indexBy(getNutsId),
+		_.mapValuesWith(getIndicatorValue)
+	]);
+	$: keyToValue = yearData && makeKeyToValue(yearData);
+
 	$: makeItems = makeItemsWithId(id);
 	$: items = yearData && makeItems(yearData);
 	$: nuts_year_spec = yearData && yearData[0].nuts_year_spec
@@ -84,6 +100,54 @@
 		_.mapValuesWith(colorScale)
 	]);
 	$: keyToColor = makeKeyToColor(items);
+	$: focusedKey = $tooltip.isVisible ? $tooltip.nuts_id : undefined;
+	$: selectedKeys = $tooltip.isVisible ? [$tooltip.nuts_id] : [];
+
+	const tooltip = writable({isVisible: false});
+
+	const makeTooltipStyle = event => {
+		const x = event.offsetX < width / 2
+		 	? {key: 'left', value: event.offsetX + 20}
+			: {key: 'right', value: width - event.offsetX + 10};
+		const y = event.offsetY < height / 2
+		 	? {key: 'top', value: event.offsetY + 20}
+			: {key: 'bottom', value: height - event.offsetY + 10};
+
+		return makeStyle({
+			[x.key]: toPx(x.value),
+			[y.key]: toPx(y.value),
+			visibility: 'visible'
+		});
+	}
+	const onEntered = event => {
+		tooltip.update(mergeObj({
+			isVisible: true,
+			nuts_id: event.detail,
+			nuts_label: keyToLabel[event.detail],
+			style: makeTooltipStyle(event),
+			value: keyToValue[event.detail]
+				? formatFn(keyToValue[event.detail])
+				: undefined
+		}))
+	};
+	const onExited = ({detail}) => {
+		tooltip.update(mergeObj({
+			isVisible: false,
+			style: 'visibility: hidden'
+		}));
+	};
+	const onMousemoved = event => {
+		$tooltip.isVisible && tooltip.update(mergeObj({
+			style: makeTooltipStyle(event),
+		}));
+	};
+
+	const onEnteredBar = ({detail: {id: focusedBarKey}}) => {
+		selectedKeys = [focusedBarKey];
+	}
+	const onExitedBar = ({detail: {id: focusedBarKey}}) => {
+		selectedKeys = [];
+	}
 </script>
 
 <svelte:head>
@@ -94,26 +158,56 @@
 	<div class="distancer">
 		<h1>{description_short} ({year})</h1>
 	</div>
-	<div class="col col1">
+	<div
+		class="col col1"
+		on:mousemove={onMousemoved}
+		bind:clientWidth={width}
+		bind:clientHeight={height}
+	>
 		{#if topojson}
 		<ChoroplethDiv
 			{keyToColor}
+			{selectedKeys}
 			{topojson}
 			colorDefaultFill='lightgrey'
 			colorStroke='black'
+			isInteractive={true}
 			key='NUTS_ID'
+			on:entered={onEntered}
+			on:exited={onExited}
 			projection='geoEqualEarth'
 			sizeStroke=0.5
 			topojsonId='NUTS'
 		/>
 		{/if}
+		{#if $tooltip.isVisible}
+		<div
+			class="tooltip"
+			style={$tooltip.style}
+		>
+			<header>
+				<span>{$tooltip.nuts_id}</span>
+				{#if $tooltip.value}
+				<span>{$tooltip.value}</span>
+				{/if}
+			</header>
+			<div>
+				<span>{$tooltip.nuts_label}</span>
+			</div>
+		</div>
+		{/if}
 	</div>
 	<div class="col col2">
 		<BarchartV
+			{focusedKey}
 			{formatFn}
 			{items}
 			{keyToColor}
 			{keyToLabel}
+			focusedKeyColor='rgb(196, 236, 255)'
+			isInteractive={true}
+			on:entered={onEnteredBar}
+			on:exited={onExitedBar}
 		/>
 	</div>
 </main>
@@ -145,10 +239,34 @@
 	.col1 {
 		grid-column: 1 / span 1;
 		overflow-y: auto;
+		position: relative;
 	}
 
 	.col2 {
 		grid-column: 2 / span 1;
-		/* border-left: 1px solid var(--color-main); */
+	}
+
+	.tooltip {
+		position: absolute;
+		pointer-events: none;
+		border: 1px solid black;
+		background-color: white;
+		color: black;
+	}
+	.tooltip header {
+		background-color: var(--color-grey-40);
+		color: white;
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		padding: 0.3rem;
+	}
+	.tooltip header span:nth-child(1) {
+		margin-right: 1rem;
+	}
+	.tooltip div {
+		display: flex;
+		align-items: center;
+		padding: 0.3rem;
 	}
 </style>
