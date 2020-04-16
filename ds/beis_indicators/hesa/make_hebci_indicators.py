8f25f077a29b59114f1bb33816302a3359442387
Juan-Mateos <juan.mateos-garcia@nesta.org.uk>
2020-04-16 15:22:14 +0100

    Modified various scripts to create and save indicators with different functions

--- ds/beis_indicators/hesa/make_hebci_indicators.py
+++ ds/beis_indicators/hesa/make_hebci_indicators.py
@@ -43,23 +43,19 @@ url_4 = 'https://www.hesa.ac.uk/data-and-analysis/providers/business-community/t
 collab = hesa_parser(url_4,'collab',skip=11)
 
 ######################
-#2. Produce and output indicators
+#2. Produce indicators
 ######################
+
+target_path = f"{project_dir}/data/processed/hebci"
 
 #Graduate startup rate
 startup_rate = calculate_perf(spin,'Number of active firms',nuts_lookup=uni_nuts,sp_def='Graduate start-ups',value='count')
 
-make_indicator(startup_rate,'hebci',{'Graduate start-ups':'total_active_graduate_startups'},
-               year_var='academic_year',decimals=0)
 
 #Turnover per spinout
 turn_per_startup = calculate_perf(spin,'Estimated current turnover of all active firms (£ thousands)',
                                   nuts_lookup=uni_nuts,norm=True,
                                   sp_def='all',value='currency')
-
-
-make_indicator(turn_per_startup,'hebci',{'gbp_all_by_company':'gbp_turnover_per_active_spinoff'},
-               year_var='academic_year',decimals=0)
 
 #Average external investment per spinout
 #This is the same as above but with investment instead of turnover. 
@@ -68,17 +64,13 @@ make_indicator(turn_per_startup,'hebci',{'gbp_all_by_company':'gbp_turnover_per_
 
 inv_per_formal = calculate_perf(spin,'Estimated external investment received (£ thousands)',norm=True,
                                 nuts_lookup=uni_nuts,sp_def='all',value='currency')
-
-make_indicator(inv_per_formal,'hebci',{'gbp_all_by_company':'gbp_investment_per_active_spinoff'},
-               year_var='academic_year',decimals=0)
-
 #Licensed income related
 ip_filter = {'category_marker':'Total IP revenues'}
 
 #Multiply by 1000 to convert into GBP
 income_nuts = 1000*make_nuts_estimate(filter_data(ip,ip_filter),uni_nuts,'value','total_ip_revenues',year_var='academic_year')
 
-make_indicator(income_nuts,'hebci',{'total_ip_revenues':'gbp_ip_revenues'},year_var='academic_year',decimals=0)
+# make_indicator(income_nuts,'hebci',{'total_ip_revenues':'gbp_ip_revenues'},year_var='academic_year',decimals=0)
 
 #Services related
 services_filter = {'type_of_service':'Consultancy','number/value_marker':'Value'}
@@ -93,14 +85,8 @@ services_nuts.columns = tidy_cols(services_nuts)
 #Select consultancy with SMEs and other companies
 services_nuts['business_consultancy'] = services_nuts.iloc[:,0]+services_nuts.iloc[:,2]
 
-make_indicator(services_nuts,'hebci',
-               {'business_consultancy':'gbp_business_consulting'},year_var='academic_year',decimals=0)
 
-#Consultancy with public sector organisations
-make_indicator(services_nuts,'hebci',{'non-commercial_organisations':'gbp_non_business_consulting'},
-               year_var='academic_year',decimals=0)
-
-#Contract research with business
+#Analysis of contract research
 contract_res_filter = {'type_of_service':'Contract research','number/value_marker':'Value'}
 
 #Note that, as before, we are multiplying by 1000 as I am dealing with businesses
@@ -112,6 +98,20 @@ res_nuts.columns = tidy_cols(res_nuts)
 #Add SME and non-SME contract research
 res_nuts['business_contract_research'] = res_nuts.iloc[:,0]+services_nuts.iloc[:,2]
 
-make_indicator(res_nuts,'hebci',{'business_contract_research':'gbp_business_contract_research'},year_var='academic_year',decimals=0)
+############
+#3. OUTPUT INDICATORS
+############
+dfs = [startup_rate,turn_per_startup,inv_per_formal,income_nuts,services_nuts,services_nuts,res_nuts]
 
+name_lookups = [{'Graduate start-ups':'total_active_graduate_startups'},
+        {'gbp_all_by_company':'gbp_turnover_per_active_spinoff'},
+        {'gbp_all_by_company':'gbp_investment_per_active_spinoff'},
+        {'total_ip_revenues':'gbp_ip_revenues'},
+        {'business_consultancy':'gbp_business_consulting'},
+        {'non-commercial_organisations':'gbp_non_business_consulting'},
+        {'business_contract_research':'gbp_business_contract_research'}]
 
+for df,name_lookup in zip(dfs,name_lookups):
+
+    ind = make_indicator(df,name_lookup,'academic_year')
+    save_indicator(ind,target_path,list(name_lookup.values())[0])
