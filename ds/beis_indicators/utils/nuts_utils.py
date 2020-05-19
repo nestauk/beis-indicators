1948011bb3a780bf517981c67ab41ad3a0adeff6
George Richardson <georgerichardson@posteo.net>
2020-05-19 10:10:20 +0100

    add load nuts_regions

--- ds/beis_indicators/utils/nuts_utils.py
+++ ds/beis_indicators/utils/nuts_utils.py
@@ -1,6 +1,9 @@
+import geopandas as gpd
 import json
 import pandas as pd
 import numpy as np
+import os
+from zipfile import ZipFile
 
 from beis_indicators.utils.set_utils import set_containment
 
@@ -105,3 +108,37 @@ def auto_nuts2_uk(df, year='year', nuts_id='nuts_id'):
 
     df = pd.concat(dfs, axis=0)
     return df
+
+
+def load_nuts_regions(year, shapefile_dir, level=2, projection=4326, resolution=1, countries=['UK']):
+    '''load_nuts_regions
+    Loads NUTS shapefiles.
+
+    Args:
+        year (int): NUTS version year.
+        shapefile_dir (str): Directory where shapefiles are stored. 
+        projection (int): Coordinate projection of shapefile. 
+            Choice of EPSG 3035, 3857 or 4326. Default is 4326
+        resolution (int): Shapefile resolution in metres.
+        countries (list): List of 2 letter country codes to filter by. If None, 
+            all regions will be returned. Default is `["UK"]`.
+    '''
+    
+    resolution = str(resolution).zfill(2)
+
+    nuts_dir = (f'{shapefile_dir}/'
+                f'ref-nuts-{year}-{resolution}m.shp/'
+                f'NUTS_RG_{resolution}M_{year}_{projection}_LEVL_{level}.shp')
+    
+    if not os.path.isdir(nuts_dir):
+        with ZipFile(f'{nuts_dir}.zip','r') as archive:
+            archive.extractall(nuts_dir)
+        
+    nuts_fin = (f'{nuts_dir}/'
+                f'NUTS_RG_{resolution}M_{year}_{projection}_LEVL_{level}.shp')
+    nuts_gdf = gpd.read_file(nuts_fin)
+    
+    if countries is not None:
+        nuts_gdf = nuts_gdf.set_index('CNTR_CODE').loc[countries].reset_index()
+        
+    return nuts_gdf
