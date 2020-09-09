a7f0f4ceb840f0df5e8ffd63395253496d18e0c1
George Richardson <georgerichardson@posteo.net>
2020-09-09 15:35:45 +0100

    add snake to camel case util

--- ds/beis_indicators/utils/__init__.py
+++ ds/beis_indicators/utils/__init__.py
@@ -1,4 +1,17 @@
 from . import pandas
+import re
+
+
+def camel_to_snake(name):
+    '''Convert lowerCamelCase or upperCamelCase to snake_case
+    Args:
+        name (str): lowerCamelCase or upperCamelCase word.
+    Returns:
+        _name (str): snake_case word.
+    '''
+    s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
+    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
+
 
 def chunks(lst, n):
     """Yield successive n-sized chunks from lst."""
