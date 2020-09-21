c8560a386fa306196dee239992a97bfa0f607c04
Luca Bonavita <mindrones@users.noreply.github.com>
2020-09-21 18:51:11 +0100

    Update ui/src/routes/guide.svelte
    Co-authored-by: George Richardson <georgerichardson@posteo.net>

--- ui/src/routes/guide.svelte
+++ ui/src/routes/guide.svelte
@@ -34,7 +34,6 @@
 
 		<h3>Timeline</h3>
 		<p>The Timeline shows one white dot for each year that the indicator is available. In this view, the timeline is interactive. Clicking on a dot navigates to a map showing the geographic distribution of the indicator for that year.<p>
-		<p>In this page, the timeline is not interactive.<p>
 
 		<h3>Sidebar</h3>
 		<p>The sidebar has the same function described above: you can use it to navigate to the trends page of the available indicators.<p></p>
