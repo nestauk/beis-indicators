501015fb900c4e0f5f770c1d12a88ac291fb407d
Luca Bonavita <mindrones@users.noreply.github.com>
2020-09-21 11:48:22 +0100

    Improve sidebar readability
    Closes #270

--- ui/src/routes/indicators/_layout.svelte
+++ ui/src/routes/indicators/_layout.svelte
@@ -50,13 +50,13 @@
 	}
 </script>
 
-<section class="container">
+<section class='layout'>
 	<nav
 		bind:this={scrollable}
 		bind:clientHeight={scrollableHeight}
 	>
 		{#each groups as {label, indicators}}
-		<div class="distancer">
+		<div class='group'>
 			<h2>{label}</h2>
 			{#each indicators as {description_short, schema}}
 			<a
@@ -78,7 +78,7 @@
 		</div>
 		{/each}
 	</nav>
-	<section class="content">
+	<section class='content'>
 		<section>
 			<slot></slot>
 		</section>
@@ -95,15 +95,20 @@
 </section>
 
 <style>
-	.container {
+	.layout {
 		height: 100%;
 		width: 100%;
 
+		--sidebarWidth: 340px;
+
 		display: grid;
-		grid-template-columns: 20% 80%;
+		grid-template-columns: var(--sidebarWidth) calc(100% - var(--sidebarWidth));
 		grid-template-rows: 100%;
 	}
-	.container > nav {
+
+	/* sidebar */
+
+	nav {
 		height: 100%;
 		width: 100%;
 		padding: var(--dim-padding);
@@ -114,23 +119,38 @@
 		color: white;
 		font-weight: var(--dim-fontsize-light);
 	}
-	.container > nav a {
+
+	nav .group:not(:last-child) {
+		margin-bottom: 1rem;
+	}
+	nav .group:not(:first-child) {
+		margin-top: 1rem;
+	}
+
+	nav h2 {
+		font-family: 'Open Sans Semibold', sans-serif ;
+		margin-bottom: 1rem;
+	}
+	nav a {
 		text-decoration: none;
 	}
-	.container > nav p {
+	nav p {
 		line-height: 1.5rem;
 		display: flex;
 		align-items: center;
-		padding-left: 0.5rem;
-    margin-bottom: 0.5rem;
+		padding: 0.4rem 0 0.4rem 0;
+		margin-bottom: 0.5rem;
 	}
-	.container > nav p.selected {
+	nav p.selected {
 		background-color: var(--color-main-desat-50) !important;
+		font-family: 'Open Sans Regular', sans-serif ;
 	}
-	.container > nav p:hover {
+	nav p:hover {
 		cursor: pointer;
 		background-color: var(--color-selected);
 	}
+
+	/* content */
 
 	.content {
 		display: grid;
