f1203f034bed28fe9a27f593c928e1779e41d7c9
Jack Rasala <jack.rasala@nesta.org.uk>
2021-05-17 17:01:08 +0200

    reformat using black

--- ds/beis_indicators/eurostat_ip/make_eurostat_ip.py
+++ ds/beis_indicators/eurostat_ip/make_eurostat_ip.py
@@ -6,66 +6,82 @@ from zipfile import ZipFile
 from io import BytesIO
 import beis_indicators
 import logging
-from beis_indicators.utils.dir_file_management import make_indicator,save_indicator
+from beis_indicators.utils.dir_file_management import make_indicator, save_indicator
 
 PROJECT_DIR = beis_indicators.project_dir
 TARGET_PATH = f"{PROJECT_DIR}/data/processed/eurostat"
 
 # We need to collect NUTS tables for 2010 (patents) and 2013 (trademarks)
-nuts_codes = {'2010':{},'2013':{}}
+nuts_codes = {"2010": {}, "2013": {}}
 
 # Only get the data if we don't have it already
-for y in ['2010','2013']:
-    file = f'NUTS_{y}.xls'
-    if os.path.exists(f'{PROJECT_DIR}/data/aux/{file}')==False:
+for y in ["2010", "2013"]:
+    file = f"NUTS_{y}.xls"
+    if os.path.exists(f"{PROJECT_DIR}/data/aux/{file}") == False:
         nuts = requests.get(
-            f'https://ec.europa.eu/eurostat/ramon/documents/nuts/NUTS_{y}.zip'
-            )
+            f"https://ec.europa.eu/eurostat/ramon/documents/nuts/NUTS_{y}.zip"
+        )
         z = ZipFile(BytesIO(nuts.content))
-        z.extract(file,path=f'{PROJECT_DIR}/data/aux/')
+        z.extract(file, path=f"{PROJECT_DIR}/data/aux/")
 
-    nuts_table = pd.read_excel(f'{PROJECT_DIR}/data/aux/{file}')
+    nuts_table = pd.read_excel(f"{PROJECT_DIR}/data/aux/{file}")
 
-    for l in [2,3]:
-        nuts_codes[y][l]=set(
-            nuts_table.loc[(nuts_table['COUNTRY CODE']=='UK')&(
-                                nuts_table['NUTS LEVEL']==l)]['NUTS CODE'])
+    for l in [2, 3]:
+        nuts_codes[y][l] = set(
+            nuts_table.loc[
+                (nuts_table["COUNTRY CODE"] == "UK") & (nuts_table["NUTS LEVEL"] == l)
+            ]["NUTS CODE"]
+        )
 
 # Collect the patent and trademark data from Eurostat
-pats = es.get_data_df('pat_ep_rtot').query("unit == 'NR'")
-high_tech_pats = es.get_data_df('pat_ep_rtec').query("unit == 'NR'")
-trades = es.get_data_df('ipr_ta_reg')
+pats = es.get_data_df("pat_ep_rtot").query("unit == 'NR'")
+high_tech_pats = es.get_data_df("pat_ep_rtec").query("unit == 'NR'")
+trades = es.get_data_df("ipr_ta_reg")
 
 # For each NUTS codes list and name
-for nuts_level,level in zip([2,3],['nuts2','nuts3']):
-    
+for nuts_level, level in zip([2, 3], ["nuts2", "nuts3"]):
+
     # For each table and variable name
-    for d,name in zip([pats,high_tech_pats,trades],
-                      ['epo_patent_applications','epo_hightech_patent_applications','eu_trademark_applications']):
-        
+    for d, name in zip(
+        [pats, high_tech_pats, trades],
+        [
+            "epo_patent_applications",
+            "epo_hightech_patent_applications",
+            "eu_trademark_applications",
+        ],
+    ):
+
         # Extract nuts codes depending on the variable (patents are 2010)
-        if 'patent' in name:
-            nuts_list = nuts_codes['2010'][nuts_level]
+        if "patent" in name:
+            nuts_list = nuts_codes["2010"][nuts_level]
         else:
-            nuts_list = nuts_codes['2013'][nuts_level]
-            
+            nuts_list = nuts_codes["2013"][nuts_level]
+
         # Select the data. We will focus on activity after 2005
-        sel = d.loc[
-            [x in nuts_list for x in d['geo\\time']]].reset_index(
-            drop=True).drop('unit',1).melt(id_vars='geo\\time').query(
-                "variable in [2006,2007,2008,2009,2010,2011,2012]")
-        
+        sel = (
+            d.loc[[x in nuts_list for x in d["geo\\time"]]]
+            .reset_index(drop=True)
+            .drop("unit", 1)
+            .melt(id_vars="geo\\time")
+            .query("variable in [2006,2007,2008,2009,2010,2011,2012]")
+        )
+
         # Make indicator
-        if 'patent' in name:
+        if "patent" in name:
             nuts_spec = 2010
         else:
             nuts_spec = 2013
 
-        ind = make_indicator(sel,{'value':name},year_var='variable',nuts_var='geo\\time',
-                    nuts_spec=nuts_spec)
-    
-        logging.info(str(min(ind['year'])))
-        logging.info(str(max(ind['year'])))
-        
+        ind = make_indicator(
+            sel,
+            {"value": name},
+            year_var="variable",
+            nuts_var="geo\\time",
+            nuts_spec=nuts_spec,
+        )
+
+        logging.info(str(min(ind["year"])))
+        logging.info(str(max(ind["year"])))
+
         # Save indicator
-        save_indicator(ind,TARGET_PATH,f"{name}.{level}")
+        save_indicator(ind, TARGET_PATH, f"{name}.{level}")
