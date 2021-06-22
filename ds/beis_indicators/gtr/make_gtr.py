b9a0b7b359aa738c87b134fb89cff9de5d438fae
Jack Rasala <47244945+Jack-0-0@users.noreply.github.com>
2021-06-22 13:00:23 +0200

    Add indicator for number of projects in all disciplines (#463)
    Closes #430.
    
    * adds back in functions for make_nuts_estimate and multiple_nuts_estimates into hesa_processing.py which are needed for make_gtr.py to run.
    
    * updates make_gtr.py to create a new indicator for number of projects in all disciplines.
    
    * adds csv and yaml files for new indicator for number of projects in all disciplines.
    
    * fixes the reverse_geocode function for the case when the user's machine username contains a '.' e.g 'john.smith'

--- ds/beis_indicators/gtr/make_gtr.py
+++ ds/beis_indicators/gtr/make_gtr.py
@@ -1,19 +1,19 @@
 import pandas as pd
-import numpy as np
-import json
+import os
 from dotenv import load_dotenv
-
 import beis_indicators
-from beis_indicators.utils.dir_file_management import *
-from beis_indicators.geo.reverse_geocoder import *
-from beis_indicators.hesa.hesa_processing import *
-
-from data_getters.inspector import get_schemas
+from beis_indicators.utils.dir_file_management import make_indicator, save_indicator
+from beis_indicators.hesa.hesa_processing import multiple_nuts_estimates
 from data_getters.core import get_engine
 from data_getters.labs.core import download_file
 
-def get_gtr(file,file_path,progress=True):
-    '''
+from beis_indicators.geo.reverse_geocoder import reverse_geocode
+
+PROJECT_DIR = beis_indicators.project_dir
+
+
+def get_gtr(file, file_path, progress=True):
+    """
     Fetch Gateway To Research predicted industries
 
     Repo: https://github.com/nestauk/gtr_data_processing
@@ -24,114 +24,150 @@ def get_gtr(file,file_path,progress=True):
         file_path (`str`, optional): Path to download to. If None, stream file.
         progress (`bool`, optional): If `True` and `file_path` is not `None`,
             display download progress.
-    '''
-    return download_file(file_to_fetch=file, download_path=file_path+file, progress=progress)
+    """
+    return download_file(
+        file_to_fetch=file, download_path=file_path + file, progress=progress
+    )
 
-def multi_geocode(source_df,lon_lat,ent_id,years):
-    '''
+
+def multi_geocode(source_df, lon_lat, ent_id, years):
+    """
     Function that extracts the nuts2 region for a location in multiple years
-    
+
     Args:
         source_df (pandas dataframe) with longitude and latitude we want to geocode
         lon_lat (list) with the names of the longitude and latitude variables
         ent_id (str) is the column we use to identify the entity we are reverse geocoding
         years (str) are the nuts years we want to extract
-        
+
     Returns a dict where the keys are the ids for a place and the values are dicts for different
     nuts years
-    '''  
-    nutified = [reverse_geocode(place_df=source_df,
-                        shape_name=f'nuts2_{str(y)}',
-                        shape_file=f'NUTS_RG_01M_{str(y)}_4326_LEVL_2.shp.zip',
-                        place_id=ent_id,
-                        coord_names= lon_lat)['NUTS_ID'] for y in years]
+    """
+    nutified = [
+        reverse_geocode(
+            place_df=source_df,
+            shape_name=f"nuts2_{str(y)}",
+            shape_file=f"NUTS_RG_01M_{str(y)}_4326_LEVL_2.shp.zip",
+            place_id=ent_id,
+            coord_names=lon_lat,
+        )["NUTS_ID"]
+        for y in years
+    ]
 
-    #Create a df and turn into a dict
-    nutified_df = pd.concat(nutified,axis=1)
+    # create a df and turn into a dict
+    nutified_df = pd.concat(nutified, axis=1)
     nutified_df.columns = [f"nuts2_{str(y)}" for y in years]
-    nutified_dict = nutified_df.to_dict(orient='index')
-    
-    return(nutified_dict)
+    nutified_dict = nutified_df.to_dict(orient="index")
+    return nutified_dict
 
-#Load the sql credentials to collect data from DAPS
+
+#########
+# 1. COLLECT DATA
+#########
+
+# load the sql credentials to collect data from DAPS
 load_dotenv()
-sql_creds = os.getenv('config_path')
+sql_creds = os.getenv("config_path")
 
-#Load list of STEM disciplines
-with open(f'{project_dir}/data/aux/stem_gtr.txt','r') as infile:
-    
-    stem = infile.read().split('\n')
+# create connection
+con = get_engine(sql_creds)
 
-#########
-#1. COLLECT DATA
-#########
-
-#Create connection
-con= get_engine(sql_creds)
-
-#1. Collect organisations-locations
-orgs_locs = pd.read_sql('gtr_organisations_locations',con=con,chunksize=1000)
+# collect organisations-locations
+orgs_locs = pd.read_sql("gtr_organisations_locations", con=con, chunksize=1000)
 orgs_locs_df = pd.concat(orgs_locs)
 
-#2. Collect projects (with discipline predictions and funding) if needed
-if os.path.exists(f"{project_dir}/data/raw/gtr/17_9_2019_gtr_projects.csv")==False:
-    get_gtr(file='17_9_2019_gtr_projects.csv',file_path=f'{project_dir}/data/raw/gtr/',
-            progress=False)
+# collect projects (with discipline predictions and funding) if needed
+if os.path.exists(f"{PROJECT_DIR}/data/raw/gtr/17_9_2019_gtr_projects.csv") == False:
+    get_gtr(
+        file="17_9_2019_gtr_projects.csv",
+        file_path=f"{PROJECT_DIR}/data/raw/gtr/",
+        progress=False,
+    )
 
-#And then we read it
-gtr_proj = pd.read_csv(f'{project_dir}/data/raw/gtr/17_9_2019_gtr_projects.csv',
-                      dtype={'id':str})
+# read projects
+gtr_proj = pd.read_csv(
+    f"{PROJECT_DIR}/data/raw/gtr/17_9_2019_gtr_projects.csv", dtype={"id": str}
+)
 
-#Keep relevant variables
-gtr_proj_short = gtr_proj[['project_id','year','amount','disc_top']]
+# keep relevant variables
+gtr_proj_short = gtr_proj[["project_id", "year", "amount", "disc_top"]]
 
-
-#3. Collect link_table connecting orgs to projects
-gtr_link = pd.read_sql('gtr_link_table',con=con,chunksize=1000)
+# collect link_table connecting orgs to projects
+# (this step takes a while)
+gtr_link = pd.read_sql("gtr_link_table", con=con, chunksize=1000)
 gtr_link_df = pd.concat(gtr_link)
-link_orgs = gtr_link_df.loc[gtr_link_df['rel']=='LEAD_ORG']
+link_orgs = gtr_link_df.loc[gtr_link_df["rel"] == "LEAD_ORG"]
 
-#This is the set of organisations that lead projects
-lead_org_id = set(link_orgs['id'])
+# this is the set of organisations that lead projects
+lead_org_id = set(link_orgs["id"])
 
 ###########
-#2. PROCESS DATA
+# 2. PROCESS DATA
 ###########
-#Create the org-nuts lookup
-#Focus on organisations that lead projects
-org_locs_lead = orgs_locs_df.loc[[x in lead_org_id for x in orgs_locs_df['id']]].reset_index(
-    drop=False)
 
-#Create the nuts lookup
-orgs_nuts = multi_geocode(org_locs_lead,['longitude','latitude'],'id',
-                          [2010,2013,2016])
+# create the org-nuts lookup
+# focus on organisations that lead projects
+org_locs_lead = orgs_locs_df.loc[
+    [x in lead_org_id for x in orgs_locs_df["id"]]
+].reset_index(drop=False)
 
-#Merge projects with organisations
+# create the nuts lookup
+orgs_nuts = multi_geocode(
+    org_locs_lead, ["longitude", "latitude"], "id", [2010, 2013, 2016]
+)
+
+# merge projects with organisations
 proj_org = pd.merge(
-    gtr_proj_short,link_orgs[['project_id','id']],
-    left_on='project_id',right_on='project_id').query('year >= 2010')
+    gtr_proj_short,
+    link_orgs[["project_id", "id"]],
+    left_on="project_id",
+    right_on="project_id",
+).query("year >= 2010")
 
 ##############
-#3. CREATE AND SAVE INDICATORS
+# 3. CREATE AND SAVE INDICATORS
 ##############
 
-# Create the NUTS estimates
-nuts_est = multiple_nuts_estimates(proj_org,
-                              orgs_nuts,
-                              set(proj_org['disc_top']),
-                              'disc_top',
-                              'amount',
-                              year_var='year',my_id='id').fillna(0)
+# create the NUTS estimates
+nuts_est = multiple_nuts_estimates(
+    proj_org,
+    orgs_nuts,
+    set(proj_org["disc_top"]),
+    "disc_top",
+    "amount",
+    year_var="year",
+    my_id="id",
+).fillna(0)
 
-#Create stem discipline aggregation
+# sum total for all disciplines
+all_nuts_est = nuts_est.sum(axis=1)
+all_nuts_est.name = "total_gtr_projects_all_disciplines"
+# make indicator for all disciplines
+ind_all = make_indicator(
+    all_nuts_est,
+    {"total_gtr_projects_all_disciplines": "total_gtr_projects_all_disciplines"},
+    "year",
+)
+# save indicator for all disciplines
+save_indicator(
+    ind_all,
+    target_path=f"{PROJECT_DIR}/data/processed/gtr",
+    var_name="total_gtr_projects_all_disciplines",
+)
+
+# load list of STEM disciplines
+with open(f"{PROJECT_DIR}/data/aux/stem_gtr.txt", "r") as infile:
+    stem = infile.read().split("\n")
+# create stem discipline aggregation
 stem_nuts_est = nuts_est[stem].sum(axis=1)
-stem_nuts_est.name = 'total_gtr_projects_stem'
-
-#Make the indicator
-ind = make_indicator(stem_nuts_est,{
-                     'total_gtr_projects_stem':'total_gtr_projects_stem'},
-                    'year')
-
-#Save the indicator
-save_indicator(ind,target_path=f'{project_dir}/data/processed/gtr',
-               var_name='total_gtr_projects_stem')
+stem_nuts_est.name = "total_gtr_projects_stem"
+# make stem indicator
+ind_stem = make_indicator(
+    stem_nuts_est, {"total_gtr_projects_stem": "total_gtr_projects_stem"}, "year"
+)
+# save stem indicator
+save_indicator(
+    ind_stem,
+    target_path=f"{PROJECT_DIR}/data/processed/gtr",
+    var_name="total_gtr_projects_stem",
+)
--- ds/beis_indicators/hesa/hesa_processing.py
+++ ds/beis_indicators/hesa/hesa_processing.py
@@ -1,4 +1,4 @@
-#Script to make HESA indicators
+# Script to make HESA indicators
 import csv
 import zipfile
 import io
@@ -9,67 +9,85 @@ import numpy as np
 import json
 
 from beis_indicators.utils.dir_file_management import *
-from beis_indicators.utils.geo_utils import leps_year_spec
-from beis_indicators.utils.nuts_utils import nuts_earliest
+
+# from beis_indicators.utils.geo_utils import leps_year_spec
+# from beis_indicators.utils.nuts_utils import nuts_earliest
 
 project_dir = beis_indicators.project_dir
 
-def filter_data(data,var_val_pairs):
-    '''
+
+def filter_data(data, var_val_pairs):
+    """
     We use this to filter the data more easily than using pandas subsetting
-    
+
     Args:
         data (df) is a dataframe
-        var_val pairs (dict) is a dictionary where the keys are variables and the value are values    
-    '''
+        var_val pairs (dict) is a dictionary where the keys are variables and the value are values
+    """
     d = data.copy()
-    
-    for k,v in var_val_pairs.items():
-        d = d.loc[d[k]==v]
-        
+
+    for k, v in var_val_pairs.items():
+        d = d.loc[d[k] == v]
+
     return d.reset_index(drop=True)
-    
-def hesa_parser(url, out_name, skip=16, encoding='utf-8'):
-    '''
-    Function to obtain and parse data from the HESA website 
-    
+
+
+def hesa_parser(url, out_name, skip=16, encoding="utf-8"):
+    """
+    Function to obtain and parse data from the HESA website
+
     Args:
         url (str) is the location of the csv file
         out_name (str) is the saved name of the file
         skip is the number of rows to skip (we could automate this by looking for rows at the top with lots of nans)
-    '''
-    if f'{out_name}.txt' not in os.listdir(f'{project_dir}/data/raw/hesa/'): 
-        #Request and parse
+    """
+    if f"{out_name}.txt" not in os.listdir(f"{project_dir}/data/raw/hesa/"):
+        # Request and parse
         rs = requests.get(url)
-        
-        #Parse the file
+
+        # Parse the file
         parsed = rs.content.decode(encoding)
-        
-        #Save it
-        
-        with open(f'{project_dir}/data/raw/hesa/{out_name}.txt','w') as outfile:
+
+        # Save it
+
+        with open(f"{project_dir}/data/raw/hesa/{out_name}.txt", "w") as outfile:
             outfile.write(parsed)
-            
-        #Read it.
-        my_csv = pd.read_csv(f'{project_dir}/data/raw/hesa/{out_name}.txt',skiprows=skip)
-        
-        #Clean column names
+
+        # Read it.
+        my_csv = pd.read_csv(
+            f"{project_dir}/data/raw/hesa/{out_name}.txt", skiprows=skip
+        )
+
+        # Clean column names
         my_csv.columns = tidy_cols(my_csv)
-        
+
         return my_csv
 
     else:
-        print(f'Already collected {out_name}')
-        out = pd.read_csv(f'{project_dir}/data/raw/hesa/{out_name}.txt',skiprows=skip,dtype={'UKPRN':str})
+        print(f"Already collected {out_name}")
+        out = pd.read_csv(
+            f"{project_dir}/data/raw/hesa/{out_name}.txt",
+            skiprows=skip,
+            dtype={"UKPRN": str},
+        )
 
         out.columns = tidy_cols(out)
 
-        #Turn the academic year into int year
-        out['academic_year'] = [parse_academic_year(x) for x in out['academic_year']]
+        # Turn the academic year into int year
+        out["academic_year"] = [parse_academic_year(x) for x in out["academic_year"]]
 
         return out
 
-def university_indicator(data, geo_data, region_type, value_header, value='number', category=None, category_col=None):
+
+def university_indicator(
+    data,
+    geo_data,
+    region_type,
+    value_header,
+    value="number",
+    category=None,
+    category_col=None,
+):
     """university_indicator
 
     Args:
@@ -78,77 +96,222 @@ def university_indicator(data, geo_data, region_type, value_header, value='numbe
         variable (str):
         select_variable (str):
         value_name (str):
-    
+
     Returns:
-        
+
     """
     if category is not None:
         data = data[data[category_col] == category]
 
-    geo_data['ukprn'] = pd.to_numeric(geo_data['ukprn'])
-    data['ukprn'] = pd.to_numeric(data['ukprn'], errors='coerce')
-    data[value] = data[value].astype('float')
-    data = data.dropna(subset=['ukprn'])
+    geo_data["ukprn"] = pd.to_numeric(geo_data["ukprn"])
+    data["ukprn"] = pd.to_numeric(data["ukprn"], errors="coerce")
+    data[value] = data[value].astype("float")
+    data = data.dropna(subset=["ukprn"])
 
-    if 'nuts' in region_type:
-        year_col = 'nuts_year_spec'
-        region_id_col = 'nuts_id'
-        groupby_year_col = 'nuts_year_spec'
-        data['year_regions'] = data['academic_year'].apply(nuts_earliest)
-    elif region_type == 'lep':
-        year_col = 'lep_year_spec'
-        groupby_year_col = 'lep_year_spec'
-        region_id_col = 'lep_id'
-        data['year_regions'] = data['academic_year'].apply(leps_year_spec)
-    
-    df = pd.merge(data, geo_data, left_on=['year_regions', 'ukprn'], 
-            right_on=[year_col, 'ukprn'])
-    df = df.groupby(['academic_year', region_id_col, groupby_year_col], as_index=False)[value].sum()
+    if "nuts" in region_type:
+        year_col = "nuts_year_spec"
+        region_id_col = "nuts_id"
+        groupby_year_col = "nuts_year_spec"
+        data["year_regions"] = data["academic_year"].apply(nuts_earliest)
+    elif region_type == "lep":
+        year_col = "lep_year_spec"
+        groupby_year_col = "lep_year_spec"
+        region_id_col = "lep_id"
+        data["year_regions"] = data["academic_year"].apply(leps_year_spec)
 
-    df = df.rename(columns={value: value_header, 'academic_year': 'year'})
-    df = df [['year', region_id_col, groupby_year_col, value_header]]
-    df = df.sort_values(by=[region_id_col, 'year'])
+    df = pd.merge(
+        data, geo_data, left_on=["year_regions", "ukprn"], right_on=[year_col, "ukprn"]
+    )
+    df = df.groupby(["academic_year", region_id_col, groupby_year_col], as_index=False)[
+        value
+    ].sum()
+
+    df = df.rename(columns={value: value_header, "academic_year": "year"})
+    df = df[["year", region_id_col, groupby_year_col, value_header]]
+    df = df.sort_values(by=[region_id_col, "year"])
     return df
 
+
 def make_student_table(url):
-    '''
+    """
     Function to create the student table
 
     Args:
         url (str) is the url with the zipfile for the student table
 
-    '''
+    """
 
-    if 'students' in os.listdir(f'{project_dir}/data/raw/hesa/'):
-        print('Already collected students table')
+    if "students" in os.listdir(f"{project_dir}/data/raw/hesa/"):
+        print("Already collected students table")
 
     else:
-        #Request
+        # Request
         rs = requests.get(url)
 
-        #Unzip and save the file
-        #Note that the file contains tables for various years. We keep all of them
-        years = ['2014-15','2015-16','2016-17','2017-18','2018-19']
+        # Unzip and save the file
+        # Note that the file contains tables for various years. We keep all of them
+        years = ["2014-15", "2015-16", "2016-17", "2017-18", "2018-19"]
 
-        out_files = [zipfile.ZipFile(io.BytesIO(rs.content)).extract(f'table-13-({year}).csv',f'{project_dir}/data/raw/hesa/students/') for year in years]
+        out_files = [
+            zipfile.ZipFile(io.BytesIO(rs.content)).extract(
+                f"table-13-({year}).csv", f"{project_dir}/data/raw/hesa/students/"
+            )
+            for year in years
+        ]
 
-        #We use a pipe to assign a year to each df and concatenate into a single df
+        # We use a pipe to assign a year to each df and concatenate into a single df
 
         graduates_all_years = pd.concat(
-            [pd.read_csv(out_files[n],skiprows=14,dtype={'UKPRN':str}) for n in np.arange(len(out_files))],axis=0)
+            [
+                pd.read_csv(out_files[n], skiprows=14, dtype={"UKPRN": str})
+                for n in np.arange(len(out_files))
+            ],
+            axis=0,
+        )
 
         graduates_all_years.columns = tidy_cols(graduates_all_years)
 
-        graduates_all_years['academic_year'] = [parse_academic_year(x) for x in graduates_all_years['academic_year']]
+        graduates_all_years["academic_year"] = [
+            parse_academic_year(x) for x in graduates_all_years["academic_year"]
+        ]
 
-        graduates_all_years.to_csv(f'{project_dir}/data/raw/hesa/students.csv',index=False)
+        graduates_all_years.to_csv(
+            f"{project_dir}/data/raw/hesa/students.csv", index=False
+        )
 
-        print('parsed collected and parsed students')
+        print("parsed collected and parsed students")
 
-def calculate_perf(table,perf,nuts_lookup,norm=False,sp_def='all',value='currency',method='time-consistent'):
-    '''
-    Function that calculates performance with HEBCI data (employment, turnover, investment, active firms...) 
-    
+
+def make_nuts_estimate(
+    data,
+    nuts_lookup,
+    counter,
+    name,
+    year_var=None,
+    method="time_consistent",
+    my_id="ukprn",
+):
+    """
+    This function takes hesa data and creates a nuts estimate
+
+    Args:
+        data (df) where we have already selected variables of interest eg mode of employment
+        nuts (dict) is the ukprn - nuts name and code lookup
+        counter (str) is the variable with counts that we are interested in
+        year_var (str) is the variable containing the years we want to group by. If None, then we are not grouping by year
+        method (str) is whether we are creating the indicator using a time consistent
+        approach (each year in its NUTS category) or using the latest nuts
+        my_id (str) is the variable with the plade id. Defaults to the university id
+
+    """
+    d = data.copy()
+
+    # Add the nuts names and codes
+
+    # If time consistent...
+    if method == "time_consistent":
+        d["nuts_code"] = [
+            nuts_lookup[row[my_id]][get_nuts_category(row[year_var])]
+            if row[my_id] in nuts_lookup.keys()
+            else np.nan
+            for rid, row in d.iterrows()
+        ]
+
+    else:
+        d["nuts_code"] = [
+            nuts_lookup[row[my_id]]["nuts2_2016"]
+            if row[my_id] in nuts_lookup.keys()
+            else np.nan
+            for rid, row in d.iterrows()
+        ]
+
+    # We are focusing on numbers
+    # Group results by year?
+    if year_var == None:
+        out = d.groupby("nuts_code")[counter].sum()
+
+    else:
+        out = d.groupby(["nuts_code", year_var])[counter].sum()
+
+    out.name = name
+
+    return out
+
+
+def multiple_nuts_estimates(
+    data,
+    nuts_lookup,
+    variables,
+    select_var,
+    value,
+    year_var=None,
+    method="time_consistent",
+    my_id="ukprn",
+):
+    """
+    Creates NUTS estimates for multiple variables.
+
+    Args:
+        data (df) is the filtered dataframe
+        select_var (str) is the variable we want to use to select values
+        nuts_lookup (dict) is the lookup between universities and nuts
+        variables (list) is the list of variables for which we want to generate the analysis
+        value (str) is the field that contains the numerical value we want to aggregate in the dataframe
+        year_var (str) is the year_variable. If none, then we are not interested in years
+        my_id (str) is the variable with the plade id. Defaults to the university id
+
+
+    """
+    if year_var == None:
+        concat = pd.concat(
+            [
+                make_nuts_estimate(
+                    data.loc[data[select_var] == m],
+                    nuts_lookup,
+                    value,
+                    m,
+                    method=method,
+                )
+                for m in variables
+            ],
+            axis=1,
+        )
+    # If we want to do this by year then we will create aggregates by nuts name and code and year and then concatenate over columns
+    else:
+
+        year_store = []
+
+        for m in variables:
+
+            y = make_nuts_estimate(
+                data.loc[data[select_var] == m],
+                nuts_lookup,
+                value,
+                m,
+                method=method,
+                year_var=year_var,
+                my_id=my_id,
+            )
+
+            year_store.append(y)
+
+        concat = pd.concat(year_store, axis=1)
+
+    return concat
+
+
+def calculate_perf(
+    table,
+    perf,
+    nuts_lookup,
+    norm=False,
+    sp_def="all",
+    value="currency",
+    method="time-consistent",
+):
+    """
+    Function that calculates performance with HEBCI data (employment, turnover, investment, active firms...)
+
     Args:
         table (df) long table with the performance and spinoff category information
         #nuts_lookup (dict) is the lookup we use for the nuts.
@@ -157,63 +320,75 @@ def calculate_perf(table,perf,nuts_lookup,norm=False,sp_def='all',value='currenc
         norm (str) if we want to normalise by the number of entities in the category
         value (str) if currency multiply by 1000 to extract gpbs
         method (str) whether we are reverse geocodign variables in a time-consistent way
-    
-    Returns a clean indicator
-    
-    '''
-    t = table.copy()
-    
-    #First get the financials
-    #Create a dict to filter the data
-    p_filter = {'metric':perf}
-    
-    #Extract the estimates
-    t_filt= multiple_nuts_estimates(filter_data(t,p_filter),nuts_lookup,set(table['category_marker']),
-                                    'category_marker','value',year_var='academic_year',method=method)
-    
 
-    #Are we subsetting by a category?
-    if sp_def == 'all':
+    Returns a clean indicator
+
+    """
+    t = table.copy()
+
+    # First get the financials
+    # Create a dict to filter the data
+    p_filter = {"metric": perf}
+
+    # Extract the estimates
+    t_filt = multiple_nuts_estimates(
+        filter_data(t, p_filter),
+        nuts_lookup,
+        set(table["category_marker"]),
+        "category_marker",
+        "value",
+        year_var="academic_year",
+        method=method,
+    )
+
+    # Are we subsetting by a category?
+    if sp_def == "all":
         t_filt = t_filt.sum(axis=1)
-    
+
     else:
         t_filt = t_filt[sp_def]
-    
-    #Tidy columns
+
+    # Tidy columns
     t_filt.name = sp_def
 
-    #Scale if the value is a currency
-    if value=='currency':
-        t_filt = t_filt*1000
-        t_filt.name = 'gbp_'+t_filt.name
-    
-    #Do the same with the totals
+    # Scale if the value is a currency
+    if value == "currency":
+        t_filt = t_filt * 1000
+        t_filt.name = "gbp_" + t_filt.name
+
+    # Do the same with the totals
     if norm == True:
-        
-        unit_filter = {'metric':'Number of active firms'}
-        
-        u_filt= multiple_nuts_estimates(filter_data(t,unit_filter),nuts_lookup,set(table['category_marker']),
-                                        'category_marker','value',year_var='academic_year',method=method)
-        
-        #Are we subsetting by a category?
-        if sp_def == 'all':
+
+        unit_filter = {"metric": "Number of active firms"}
+
+        u_filt = multiple_nuts_estimates(
+            filter_data(t, unit_filter),
+            nuts_lookup,
+            set(table["category_marker"]),
+            "category_marker",
+            "value",
+            year_var="academic_year",
+            method=method,
+        )
+
+        # Are we subsetting by a category?
+        if sp_def == "all":
             u_filt = u_filt.sum(axis=1)
 
         else:
             u_filt = u_filt[sp_def]
 
-        #Tidy columns
-        u_filt.name = 'all_comps'
-        
-        comb = pd.concat([t_filt,u_filt],axis=1)
-        
-        comb[f'{t_filt.name}_by_company']= comb[t_filt.name]/comb['all_comps']
-        
-        #Zeroes are nans (this is to avoid division by zero)
-        comb.fillna(0,inplace=True)
-        
-        return(comb)
-    
+        # Tidy columns
+        u_filt.name = "all_comps"
+
+        comb = pd.concat([t_filt, u_filt], axis=1)
+
+        comb[f"{t_filt.name}_by_company"] = comb[t_filt.name] / comb["all_comps"]
+
+        # Zeroes are nans (this is to avoid division by zero)
+        comb.fillna(0, inplace=True)
+
+        return comb
+
     else:
-        return(t_filt)
-        
+        return t_filt
