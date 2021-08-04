78c4df6c0a82cf9e3d87dd8cbc64b5df38069223
Sebastian Ferreyra Pons <ushiferreyra@gmail.com>
2021-08-04 11:15:16 -0300

    Scrolling fix

--- ui/src/routes/guides/a11ymenu.svelte
+++ ui/src/routes/guides/a11ymenu.svelte
@@ -12,137 +12,135 @@
 	>
 </svelte:head>
 
-<main>
-	<section>
-		<h2>What the accessibility menu does</h2>
+<section>
+	<h2>What the accessibility menu does</h2>
 
+	<p>
+		You can use the menu to change two groups of features.
+	</p>
+
+	<h3>Text features</h3>
+
+	<p>You can change:</p>
+
+	<ul>
+		<li>
+			the font (in particular we provide a font specific to help
+			people with Dyslexia),
+		</li>
+		<li>the font size,</li>
+		<li>
+			the line height (distance between lines of text in a paragraph),
+		</li>
+		<li>the letter spacing (distance between letters in a word),</li>
+		<li>the word spacing (distance between words).</li>
+	</ul>
+
+	<h3>Color features</h3>
+
+	<p>You can:</p>
+
+	<ul>
+		<li>invert all the colors in the app,</li>
+		<li>
+			select a color vision deficiency: this will remap all the colors
+			in the app to compensate for a diminished ability to see certain
+			colors,
+		</li>
+		<li>
+			edit the hue of all the colors in the app,
+		</li>
+		<li>
+			edit the saturation of all the colors in the app (by editing
+			their level of grey),
+		</li>
+		<li>edit the brightness of all the colors in the app,</li>
+		<li>edit the contrast of all the colors in the app.</li>
+	</ul>
+
+	<h2>How to use the menu</h2>
+
+	<h3>How to toggle the menu on/off</h3>
+
+	<p>
+		To show or hide the menu, you need to click the accessibility
+		icon in the top-right corner of the app.
+	</p>
+
+	<h3>Where to find the menu</h3>
+
+	{#if $_screen?.sizes.medium}
 		<p>
-			You can use the menu to change two groups of features.
+			The menu should appear at the center of the screen
+			sligthly toward the bottom.
 		</p>
-
-		<h3>Text features</h3>
-
-		<p>You can change:</p>
-
-		<ul>
-			<li>
-				the font (in particular we provide a font specific to help
-				people with Dyslexia),
-			</li>
-			<li>the font size,</li>
-			<li>
-				the line height (distance between lines of text in a paragraph),
-			</li>
-			<li>the letter spacing (distance between letters in a word),</li>
-			<li>the word spacing (distance between words).</li>
-		</ul>
-
-		<h3>Color features</h3>
-
-		<p>You can:</p>
-
-		<ul>
-			<li>invert all the colors in the app,</li>
-			<li>
-				select a color vision deficiency: this will remap all the colors
-				in the app to compensate for a diminished ability to see certain
-				colors,
-			</li>
-			<li>
-				edit the hue of all the colors in the app,
-			</li>
-			<li>
-				edit the saturation of all the colors in the app (by editing
-				their level of grey),
-			</li>
-			<li>edit the brightness of all the colors in the app,</li>
-			<li>edit the contrast of all the colors in the app.</li>
-		</ul>
-
-		<h2>How to use the menu</h2>
-
-		<h3>How to toggle the menu on/off</h3>
-
+	{:else}
 		<p>
-			To show or hide the menu, you need to click the accessibility
-			icon in the top-right corner of the app.
+			The menu should appear below the navigation header, near the
+			thumb.
 		</p>
+	{/if}
 
-		<h3>Where to find the menu</h3>
+	<h3>How to navigate the menu</h3>
 
-		{#if $_screen?.sizes.medium}
-			<p>
-				The menu should appear at the center of the screen
-				sligthly toward the bottom.
-			</p>
-		{:else}
-			<p>
-				The menu should appear below the navigation header, near the
-				thumb.
-			</p>
-		{/if}
+	<p>The menu is composed of three sections:</p>
 
-		<h3>How to navigate the menu</h3>
+	<ul>
+		<li>
+			the main area, in the center, shows the controls to manipulate a
+			certain feature,
+		</li>
+		<li>
+			on the right, you have two buttons to navigate to the previous
+			or next feature,
+		</li>
+		<li>
+			on the left, you have two buttons to reset all the features of a
+			particular group (text or color).
+		</li>
+	</ul>
 
-		<p>The menu is composed of three sections:</p>
+	<h3>Editing features</h3>
 
-		<ul>
-			<li>
-				the main area, in the center, shows the controls to manipulate a
-				certain feature,
-			</li>
-			<li>
-				on the right, you have two buttons to navigate to the previous
-				or next feature,
-			</li>
-			<li>
-				on the left, you have two buttons to reset all the features of a
-				particular group (text or color).
-			</li>
-		</ul>
+	<h4>List of values</h4>
 
-		<h3>Editing features</h3>
+	<p>
+		When you have a list of options, you might get presented with a
+		series of radio buttons. For small screens, the list of radio
+		buttons will be just two buttons:
+	</p>
 
-		<h4>List of values</h4>
+	<ul>
+		<li>for numeric values, these will be a plus and a minus button</li>
+		<li>
+			otherwise, these will be two left/right arrows that navigate to
+			the previous/next option, respectively.
+		</li>
+	</ul>
 
-		<p>
-			When you have a list of options, you might get presented with a
-			series of radio buttons. For small screens, the list of radio
-			buttons will be just two buttons:
-		</p>
+	<p>For example, you can choose multiple font sizes.</p>
 
-		<ul>
-			<li>for numeric values, these will be a plus and a minus button</li>
-			<li>
-				otherwise, these will be two left/right arrows that navigate to
-				the previous/next option, respectively.
-			</li>
-		</ul>
+	<h4>Pair of values</h4>
 
-		<p>For example, you can choose multiple font sizes.</p>
+	<p>
+		When you have only two opposite options you get presented with a
+		switch.
+	</p>
 
-		<h4>Pair of values</h4>
+	<p>For example, you can invert all colors of the app, or not.</p>
 
-		<p>
-			When you have only two opposite options you get presented with a
-			switch.
-		</p>
+	<h4>Range of values</h4>
 
-		<p>For example, you can invert all colors of the app, or not.</p>
+	<p>
+		When the feature you are editing is defined by a numerical range,
+		you can select any value in between the range extent using a slider.
+	</p>
 
-		<h4>Range of values</h4>
-
-		<p>
-			When the feature you are editing is defined by a numerical range,
-			you can select any value in between the range extent using a slider.
-		</p>
-
-		<p>
-			For example, you can change the saturation gradually, from no change
-			to grey.
-		</p>
-	</section>
-</main>
+	<p>
+		For example, you can change the saturation gradually, from no change
+		to grey.
+	</p>
+</section>
 
 <style>
 	section {
